e2f1646c155aa82c406a5c8282d0f7cd5b8fcb5c
SamPriceCode <sampriceschool@gmail.com>
2023-10-18 23:55:53 -0400

    general improvements

--- schedulers.cpp
+++ schedulers.cpp
@@ -4,28 +4,41 @@
 
 using namespace std;
 
-//general instructions
+//constants
 namespace sch {
+	const int P1[] = { 5, 27, 3, 31, 5, 43, 4, 18, 6, 22, 4, 26, 3, 24, 4 };
+	const int P2[] = { 4, 48, 5, 44, 7, 42, 12, 37, 9, 76, 4, 41, 9, 31, 7, 43, 8 };
+	const int P3[] = { 8, 33, 12, 41, 18, 65, 14, 21, 4, 61, 15, 18, 14, 26, 5, 31, 6 };
+	const int P4[] = { 3, 35, 4, 41, 5, 45, 3, 51, 4, 61, 5, 54, 6, 82, 5, 77, 3 };
+	const int P5[] = { 16, 24, 17, 21, 5, 36, 16, 26, 7, 31, 13, 28, 11, 21, 6, 13, 3, 11, 4 };
+	const int P6[] = { 11, 22, 4, 8, 5, 10, 6, 12, 7, 14, 9, 18, 12, 24, 15, 30, 8 };
+	const int P7[] = { 14, 46, 17, 41, 11, 42, 15, 21, 4, 32, 7, 19, 16, 33, 10 };
+	const int P8[] = { 4, 14, 5, 33, 6, 51, 14, 73, 16, 87, 6 };
+
+	const int* PROGRAMS[] = { P1, P2, P3, P4, P5, P6, P7, P8 };
+
+	const int NEW = 0, READY = 1, CPU = 2, IO = 3, TERMINATE = 4;
+
+}
+
+//simulation functions
+namespace sch{
 	void loadPrograms(vector<PCB>& readyQ) {
 		for (int i = 0; i < 8; i++) {
 			PCB process;
 			process.state = READY;
-			process.number = i;
+			process.number = i+1;
 			process.instructions = PROGRAMS[i];
+			process.length = getInstructionLength(PROGRAMS[i], process.number);
 			readyQ.push_back(process);
 		}
 
 		cout << "\nPrograms Loaded Sucessfully";
 	}
 
-	void addWait(vector<PCB>& readyQ) {
+	void addTime(vector<PCB>& readyQ) {
 		for (int i = 0; i < readyQ.size(); i++) {
 			readyQ[i].waittime++;
-		}
-	}
-
-	void addResponse(vector<PCB>& readyQ) {
-		for (int i = 0; i < readyQ.size(); i++) {
 			if (readyQ[i].accessed == false) {
 				readyQ[i].responsetime++;
 			}
@@ -42,67 +55,104 @@ namespace sch {
 	void dismissIO(vector<PCB>& ioQ, vector<PCB>& readyQ) {
 		//cout << "\n\t\t\t\tDismiss IO & sizeL:" << ioQ.size() << endl;
 		for (int i = 0; i < ioQ.size(); i++) {
-			cout << "\n\t\t Process " << i << " I/O burst time: " << ioQ[i].ioBurst;
-			if (ioQ[i].ioBurst < 1) {
+			//cout << "\n\t\t Process " << ioQ[i].number << " I/O burst time: " << ioQ[i].ioBurst;
+			if (ioQ[i].ioBurst <= 0) {
 				readyQ.push_back(ioQ[i]);
-				ioQ.erase(ioQ.begin());
+				ioQ.erase(ioQ.begin()+i);
 			}
+		}
+	}
+
+	void checkIO(vector<PCB>& ioQ) {
+		//cout << "\n\t\t\t\tDismiss IO & sizeL:" << ioQ.size() << endl;
+		for (int i = 0; i < ioQ.size(); i++) {
+			cout << "\n\t\t Process " << ioQ[i].number << " I/O Time: " << ioQ[i].ioBurst;
+		}
+	}
+
+	int getInstructionLength(const int process[], int number) {
+		int i = 0, length = 0;
+		cout << "\nP" << number << ": { ";
+		while (process[i]) {
+			cout << process[i] << " ";
+			length++;
+			i++;
+		}
+		cout << "}";
+		return length;
+	}
+
+	void processLengths(vector<PCB>& readyQ) {
+		cout << "Process instructions lengths:";
+		for (int i = 0; i < readyQ.size(); i++) {
+			cout << "\n\tProcess " << i << ": " << readyQ[i].length;
 		}
 	}
 }
 
-//First Come First Serve
+//schedulers
 namespace sch {
 	void FirstComeFirstServe() {
 		int clock, readypos = 0, procpos = 0,
-			runtime = 0;
+			cputime = 0;
 		bool ioburst;
-		vector<PCB> readyQ, ioQ, terminateQ;
+		vector<PCB> readyQ, cpuQ, ioQ, terminateQ;
 
 		vector<int> ioBurst;
-		PCB cpu;
 		loadPrograms(readyQ);
 
+
+		processLengths(readyQ);
+
 		clock = 0;
-		cout << "\nStart Simulation.";
+		cout << "\n---------------------Start Simulation---------------------";
 		while (true) {
 			cout << "\nClock #: " << clock
-				<< " - ready queue size: " << readyQ.size()
+				<< " - Ready queue size: " << readyQ.size()
 				<< " - I/O queue size: " << ioQ.size();
 			if (readyQ.size() > 0) {
-				clock++;
-				addWait(readyQ);
-				addResponse(readyQ);
-
-				cpu = readyQ[0];
-				cpu.accessed = true;
+				cpuQ.push_back(readyQ[0]);
+				cpuQ[0].accessed = true;
 				readyQ.erase(readyQ.begin());
 			}
 
-			runtime = 0;
+			cputime = 0;
 
-			cout << "\n\tProcessing Program " << cpu.number;
+			cout << "\n\tProcessing: P" << cpuQ[0].number << " instruction #" << cpuQ[0].counter << " for " << cpuQ[0].instructions[cpuQ[0].counter] << " cycles.";
 
-			while (runtime < cpu.instructions[cpu.counter]) {
-				addWait(readyQ);
-				addResponse(readyQ);
+			checkIO(ioQ);
+			while (cputime < cpuQ[0].instructions[cpuQ[0].counter]) {
+				if (readyQ.size() > 0) {
+					addTime(readyQ);
+				}
 				clock++;
-				runtime++;
+				cputime++;
 				decreaseIO(ioQ);
+				dismissIO(ioQ, readyQ);
 			}
-			cout << "\n\tCycle length: " << runtime;
+			cout << "\n\tCycle length: " << cputime;
+			checkIO(ioQ);
 
-			cpu.counter++;
+			cpuQ[0].counter++;
 
-			if (cpu.counter > sizeof(cpu.instructions)) {
+			cout << "\nCpu counter/instructions: " << cpuQ[0].counter << "/" << cpuQ[0].length;
+
+			if (cpuQ[0].counter >= cpuQ[0].length) {
+				cout << "\n\tTerminating process P" << cpuQ[0].number;
+				terminateQ.push_back(cpuQ[0]);
+				cpuQ.erase(cpuQ.begin());
 			}
 
-			ioQ.push_back(cpu);
-			ioQ.back().ioBurst = ioQ.back().instructions[ioQ.back().counter];
 
+			if (cpuQ.size()) {
+				ioQ.push_back(cpuQ[0]);
+				ioQ.back().ioBurst = ioQ.back().instructions[ioQ.back().counter];
+				cpuQ.erase(cpuQ.begin());
+			}
 
-			dismissIO(ioQ, readyQ);
+			cout << "\nTerminate queue size: " << terminateQ.size();
 			cout << endl;
+
 
 			if (readyQ.size() < 1) {
 				break;
--- schedulers.h
+++ schedulers.h
@@ -8,22 +8,23 @@
 using namespace std;
 
 namespace sch {
-	extern const int P1[15];
-	extern const int P2[17];
-	extern const int P3[17];
-	extern const int P4[17];
-	extern const int P5[19];
-	extern const int P6[17];
-	extern const int P7[15];
-	extern const int P8[11];
+	/*extern const int P1[];
+	extern const int P2[];
+	extern const int P3[];
+	extern const int P4[];
+	extern const int P5[];
+	extern const int P6[];
+	extern const int P7[];
+	extern const int P8[];
 
-	extern const int* PROGRAMS[8];
+	extern const int* PROGRAMS[];*/
 
 	extern const int NEW, READY, CPU, IO, TERMINATE;
 
 	struct PCB {
 		int number;
 		const int* instructions;
+		int length;
 		int state = NEW;
 		int waittime = 0;
 		int responsetime = 0;
@@ -33,16 +34,20 @@ namespace sch {
 	};
 	
 	//First Come First Serve
-	void FirstComeFirstServe();
+		void FirstComeFirstServe();
 
 
-	//General CPU commands
-	void loadPrograms(vector<PCB>&);
-	void addWait(vector<PCB>&);
-	void addResponse(vector<PCB>&);
+	//General simulation commands
+		void loadPrograms(vector<PCB>&);
+		void addTime(vector<PCB>&);
 
-	void decreaseIO(vector<PCB>&);
-	void dismissIO(vector<PCB>&, vector<PCB>&);
+		void decreaseIO(vector<PCB>&);
+		//input ioQ and readyQ, returns total dismissals
+		void dismissIO(vector<PCB>&, vector<PCB>&);
+		void checkIO(vector<PCB>&);
+
+		int getInstructionLength(const int process[], int);
+		void processLengths(vector<PCB>&);
 }
 
 #endif
